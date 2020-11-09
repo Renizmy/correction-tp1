76cb22978ac80a2c920e7c4d4865729116888043
remi <remi.beltramini@gmail.com>
2020-11-09 18:14:26 +0100

    another commit

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,9 +4,8 @@ docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
   
  #build
 docker build -t $DOCKER_USERNAME/tp2-devops:tp_database database
-docker build -t $DOCKER_USERNAME/tp2-devops:tp_simple-api simpleapi
+docker build -t $DOCKER_USERNAME/tp2-devops:tp_simple-api simple-api
 docker build -t $DOCKER_USERNAME/tp2-devops:tp_httpd httpd
-
 docker push $DOCKER_USERNAME/tp2-devops:tp_database
 docker push $DOCKER_USERNAME/tp2-devops:tp_simple-api
 docker push $DOCKER_USERNAME/tp2-devops:tp_httpd
