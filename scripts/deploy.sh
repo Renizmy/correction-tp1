76e90513f7e5bd0e004ab30f132758aa21d17e57
remi <remi.beltramini@gmail.com>
2020-11-10 08:09:38 +0100

    another commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $ssh_access >> ~/.id_rsa
-ssh -p 500 -i ~/.id_rsa remi@94.239.44.47 ls -la ; ansible-playbook -i /home/remi/Documents/correction-tp1/ansible/inventories/setup.yml /home/remi/Documents/correction-tp1/ansible/playbook.yml
+ssh -o StrictHostKeyChecking=no -p 500 -i ~/.id_rsa remi@94.239.44.47 ls -la ; ansible-playbook -i /home/remi/Documents/correction-tp1/ansible/inventories/setup.yml /home/remi/Documents/correction-tp1/ansible/playbook.yml
 
 
 
