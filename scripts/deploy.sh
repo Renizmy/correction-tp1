9424441018694a3fbb1cd4e369c47f2b4fa55c4d
remi <remi.beltramini@gmail.com>
2020-11-10 11:27:08 +0100

    ssefe

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install sshpass
-sshpass -p $password ssh remi@94.239.44.47
-ansible-playbook -i /home/remi/Documents/correction-tp1/ansible/inventories/setup.yml /home/remi/Documents/correction-tp1/ansible/playbook.yml
-
-
-
