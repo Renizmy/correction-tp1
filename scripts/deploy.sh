0436a1e0363d732f28be3fdc30830e809c23e51e
remi <remi.beltramini@gmail.com>
2020-11-16 03:46:25 +0100

    Last one ?

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-echo $vault_pass >> ansible/.vault_pass
+echo $vault_pass >> ansible/vault_pass
 ansible-vault view ansible/key/id_rsa_encrypted >> id_rsa
 chmod 400 id_rsa
 ansible-playbook -i ansible/inventories/setup.yml ansible/playbook.yml
