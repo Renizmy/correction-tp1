9debd6fe8cbff5643e73d6925375d309e6ab04df
remi <remi.beltramini@gmail.com>
2020-11-16 01:29:11 +0100

    one more

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!bin/bash
-touch .vault_pass
-echo $vault_pass >> .vault_pass
-ansible-vault view encrypted/id_rsa_encrypted >> id_rsa
+echo $vault_pass >> ansible/.vault_pass
+ansible-vault view ansible/key/id_rsa_encrypted >> id_rsa
 chmod 400 id_rsa
 ansible-playbook -i ansible/inventories/setup.yml ansible/playbook.yml
